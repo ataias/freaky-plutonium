7d53f35cc7f6c240ff24b61b92e90f0222bb4cb2
Ataias Pereira Reis <ataias@brickabode.com>
2019-08-11 09:59:10 -0300

    Add more functions

--- Program.fs
+++ Program.fs
@@ -1,21 +1,14 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 open System
 open System.Diagnostics
 
-let make_lynx_args (base_link:string) : string =
-    [ "-display_charset=UTF-8";
-        "-dump";
-        base_link
-      ] |> List.fold (fun x y -> x + " " + y) ""
-
-let download_page (link:string) : string =
-    let prog_and_args = make_lynx_args link
-
+/// Runs programs and gets standard output in variable
+let run_cmd (cmd:string) (args:string) : string =
     let proc = new Process(
                 StartInfo = ProcessStartInfo(
-                    FileName = "lynx",
-                    Arguments = prog_and_args,
+                    FileName = cmd,
+                    Arguments = args,
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
                     CreateNoWindow = true
@@ -24,6 +17,16 @@ let download_page (link:string) : string =
     proc.Start() |> ignore
     proc.StandardOutput.ReadToEnd()
 
+let make_lynx_args (base_link:string) : string =
+    [ "-display_charset=UTF-8";
+        "-dump";
+        base_link
+      ] |> List.fold (fun x y -> x + " " + y) ""
+
+let download_page (link:string) : string =
+    let args = make_lynx_args link
+    run_cmd "lynx" args
+
 let download_reverso (word:string) : string =
     let reverso_base = "https://context.reverso.net/translation/french-english/{0}"
     let link = String.Format(reverso_base, word)
@@ -31,6 +34,5 @@ let download_reverso (word:string) : string =
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
     printfn "%s" (download_reverso "aveuglement")
     0 // return an integer exit code
